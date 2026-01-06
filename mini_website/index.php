c76bce6533b1d648647d52e7849671745a9aa219
Anthony L <laplane_anthony@yahoo.fr>
2026-01-06 15:48:18 +0100

    minw

---
+++ mini_website/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="about.php">A propos</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Accueil</h1>
+    </main>
+    <footer>
+        Copyright MiniWebsite
+    </footer>
+</body>
+</html>
