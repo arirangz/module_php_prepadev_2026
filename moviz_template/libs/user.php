58a6c5e177ac752c24d245a69c2de58d23886f66
Anthony L <laplane_anthony@yahoo.fr>
2026-01-14 12:24:30 +0100

    connexion à partir de la bdd

--- moviz_template/libs/user.php
+++ moviz_template/libs/user.php
@@ -1,12 +1,21 @@
 <?php
 
-function loginUser(string $email, string $password):bool
+function loginUser(PDO $pdo, string $email, string $password):bool
 {
-    if  ($email === "test@gmail.com" && $password === "ABC123"){
+    // On tente de récupérer un utilisateur à partir de l'email
+    $query = $pdo->prepare("SELECT * FROM user WHERE email = :email");
+    $query->bindValue(":email", $email);
+    $query->execute();
+
+    $user = $query->fetch(PDO::FETCH_ASSOC);
+
+
+    if  ($user && password_verify($password, $user["password"])){
         //regénéré l'id de session (sécurité: fixation de session)
         session_regenerate_id(true);
         //On stock l'email dans la session
-        $_SESSION["email"] = $email;
+        $_SESSION["email"] = $user["email"];
+        $_SESSION["nickname"] = $user["nickname"];
         return true;
     }
     return false;
