058d8189fc95db88202104efb57796faab4e51b6
Anthony L <laplane_anthony@yahoo.fr>
2026-01-14 09:49:11 +0100

    gestion d'erreurs

--- moviz_template/movie_details.php
+++ moviz_template/movie_details.php
@@ -3,20 +3,46 @@
 require_once "libs/movie.php";
 require_once "templates/header.php"; 
 
-$index = $_GET["index"];
-$movie = getMovieByIndex($index);
+$error = false;
+
+if (isset($_GET["index"])) {
+    $index = $_GET["index"];
+    if (is_numeric($index)) {
+        $movie = getMovieByIndex((int)$index);
+        
+        if (!$movie) {
+            // Le film n'a pas été trouvé dans le tableau
+            $error = true;
+        }
+    } else {
+        // L'index n'était pas numérique
+        $error = true;
+    }
+} else {
+    // Si l'index n'est pas présent dans l'url
+    $error = true;
+}
+
+
 
 ?>
 
 <div class="container col-xxl-8 px-4 py-5">
-    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div class="col-10 col-sm-8 col-lg-6"> <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"> </div>
-        <div class="col-lg-6">
-            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3"><?=htmlspecialchars($movie["title"]);  ?> </h1>
-            <p class="lead"><?= htmlspecialchars($movie["summary"]) ?></p>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-start"> <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button> <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button> </div>
+
+    <?php if ($error):?>
+        <div class="alert alert-danger" role="alert">
+        Film introuvable
         </div>
-    </div>
+    <?php else: ?>
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="col-10 col-sm-8 col-lg-6"> <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"> </div>
+            <div class="col-lg-6">
+                <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3"><?=htmlspecialchars($movie["title"]);  ?> </h1>
+                <p class="lead"><?= htmlspecialchars($movie["summary"]) ?></p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start"> <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button> <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button> </div>
+            </div>
+        </div>
+    <?php endif;?>
 </div>
 
 
